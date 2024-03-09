07e15c4235e257ace595f22c4bf21adeee4515d8
Liger <gjfleo@yeah.net>
2024-03-09 15:17:07 +0800

    feat: 添加音乐

---
+++ src/utils/albums/20220413 佚落迁忘之岛.ts
@@ -0,0 +1,63 @@
+import type { IMAlbum } from "../types";
+
+export default {
+  title: "佚落迁忘之岛",
+  titleEn: "Islands of the Lost and Forgotten",
+  musics: [
+    {
+      tracks: [
+        {
+          title: "雷霆的威光",
+          titleEn: "Thunderings of the Merciless",
+          scene: "阶段1",
+          start: 0,
+          links: {
+            ne: 1937111535,
+            qq: "002r4wwr3zrU53",
+          },
+          credits: [
+            ["作曲", "陈致逸 (HOYO-MiX)"],
+            ["编曲", "赵鑫 (HOYO-MiX)"],
+            ["乐队", "国际首席爱乐乐团"],
+            ["尺八", "寄田 真見乃"],
+            ["筝", "渡邊 香澄"],
+            ["钢琴", "陈致逸 (HOYO-MiX)"],
+            ["录音棚", "Heartbeat Recording Studio", "金田录音棚"],
+            ["音乐监制", "千葉 智史 (Sony Music Publishing (Japan) Inc.)"],
+            ["录音师", "吉羽 貴史 (Smart Break Sound)", "王小四"],
+            ["混音师", "黄巍"],
+            ["母带制作", "黄巍"],
+            ["联合制作", "Sony Music Entertainment (Japan) Inc."],
+            ["出品", "HOYO-MiX"],
+          ],
+        },
+        {
+          title: "稻光神鸣",
+          titleEn: "The Almighty Violet Thunder",
+          scene: "阶段2",
+          start: 0,
+          links: {
+            ne: 1937111527,
+            qq: "0018qGcb3o7IjK",
+          },
+          credits: [
+            ["作曲", "陈致逸 (HOYO-MiX)"],
+            ["编曲", "赵鑫 (HOYO-MiX)"],
+            ["乐队", "国际首席爱乐乐团"],
+            ["尺八", "寄田 真見乃"],
+            ["筝", "渡邊 香澄"],
+            ["电吉他", "赵鑫 (HOYO-MiX)"],
+            ["钢琴", "陈致逸 (HOYO-MiX)"],
+            ["录音棚", "Heartbeat Recording Studio", "金田录音棚"],
+            ["音乐监制", "千葉 智史 (Sony Music Publishing (Japan) Inc.)"],
+            ["录音师", "吉羽 貴史 (Smart Break Sound)", "王小四"],
+            ["混音师", "黄巍"],
+            ["母带制作", "黄巍"],
+            ["联合制作", "Sony Music Entertainment (Japan) Inc."],
+            ["出品", "HOYO-MiX"],
+          ],
+        },
+      ],
+    },
+  ],
+} satisfies IMAlbum;
--- src/utils/albums/index.ts
+++ src/utils/albums/index.ts
@@ -1,6 +1,7 @@
 import type { IMAlbum } from "../types";
 
 const albums: IMAlbum[] = [
+  (await import("./20220413 佚落迁忘之岛")).default,
   (await import("./20220622 千岩旷望")).default,
 ];
 
